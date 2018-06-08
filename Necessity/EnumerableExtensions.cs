958141ee61ed11b136f5350d986254e49847fb24
Thomas Andresen <thomas.andresen@esmartsystems.com>
2018-06-08 15:01:19 +0200

    Add partition ienumerable extension

--- Necessity/EnumerableExtensions.cs
+++ Necessity/EnumerableExtensions.cs
@@ -9,5 +9,26 @@ namespace Necessity
         {
             return target as ICollection<T> ?? target.ToList();
         }
+
+        public static IEnumerable<List<T>> Partition<T>(this IEnumerable<T> source, int partitionSize)
+        {
+            var toReturn = new List<T>(partitionSize);
+            foreach (var item in source)
+            {
+                toReturn.Add(item);
+
+                if (toReturn.Count != partitionSize)
+                {
+                    continue;
+                }
+
+                yield return toReturn;
+                toReturn = new List<T>(partitionSize);
+            }
+            if (toReturn.Any())
+            {
+                yield return toReturn;
+            }
+        }
     }
 }
