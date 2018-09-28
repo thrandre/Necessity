cd9bea8d39331f0d27d333fc683b2c9de90df3b9
Thomas Andresen <thomas.andresen@esmartsystems.com>
2018-09-28 13:10:32 +0200

    Add support  for B64Url Encoding

---
+++ Necessity/Base64UrlEncoding.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Necessity
+{
+    public static class Base64UrlEncoding
+    {
+        public static string Encode(byte[] arg)
+        {
+            string s = Convert.ToBase64String(arg); // Standard base64 encoder
+
+            s = s.Split('=')[0]; // Remove any trailing '='s
+            s = s.Replace('+', '-'); // 62nd char of encoding
+            s = s.Replace('/', '_'); // 63rd char of encoding
+
+            return s;
+        }
+
+        public static byte[] Decode(string arg)
+        {
+            string s = arg;
+            s = s.Replace('-', '+'); // 62nd char of encoding
+            s = s.Replace('_', '/'); // 63rd char of encoding
+
+            switch (s.Length % 4) // Pad with trailing '='s
+            {
+                case 0: break; // No pad chars in this case
+                case 2: s += "=="; break; // Two pad chars
+                case 3: s += "="; break; // One pad char
+                default: throw new Exception("Illegal base64url string!");
+            }
+
+            return Convert.FromBase64String(s); // Standard base64 decoder
+        }
+    }
+
+    public static class JsonB64UrlEncoding
+    {
+        public static string Pack<T>(T model)
+        {
+            return JsonConvert
+                .SerializeObject(model)
+                .Pipe(Encoding.UTF8.GetBytes)
+                .Pipe(Base64UrlEncoding.Encode);
+        }
+
+        public static T Unpack<T>(string model)
+        {
+            return model
+                .Pipe(Base64UrlEncoding.Decode)
+                .Pipe(b => Encoding.UTF8.GetString(b, 0, b.Length))
+                .Pipe(JsonConvert.DeserializeObject<T>);
+        }
+    }
+}
