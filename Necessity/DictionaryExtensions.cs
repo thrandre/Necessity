f1444645b72b243bb6dbd493a07301d1dc3d1bcf
Thomas Andresen <thomas.r.andresen@gmail.com>
2019-05-06 14:35:27 +0200

    Add support for iequalitycomparer in dictextensions

--- Necessity/DictionaryExtensions.cs
+++ Necessity/DictionaryExtensions.cs
@@ -43,9 +43,9 @@ namespace Necessity
             target.Add(key, updateFn(key, default(TVal)));
         }
 
-        public static Dictionary<TKey, TVal> ToNonCollidingDictionary<T, TKey, TVal>(this IEnumerable<T> target, Func<T, TKey> keySelector, Func<T, TVal> valueSelector, Func<TVal, TVal, TVal> collisionResolver)
+        public static Dictionary<TKey, TVal> ToNonCollidingDictionary<T, TKey, TVal>(this IEnumerable<T> target, Func<T, TKey> keySelector, Func<T, TVal> valueSelector, Func<TVal, TVal, TVal> collisionResolver, IEqualityComparer<TKey> comparer = null)
         {
-            var dict = new Dictionary<TKey, TVal>();
+            var dict = new Dictionary<TKey, TVal>(comparer);
 
             foreach (var item in target)
             {
@@ -60,7 +60,7 @@ namespace Necessity
             return dict;
         }
 
-        public static Dictionary<TKey, TVal> ToNonCollidingDictionary<T, TKey, TVal>(this IEnumerable<T> target, Func<T, TKey> keySelector, Func<T, TVal> valueSelector, ResolveOption resolveOption)
+        public static Dictionary<TKey, TVal> ToNonCollidingDictionary<T, TKey, TVal>(this IEnumerable<T> target, Func<T, TKey> keySelector, Func<T, TVal> valueSelector, ResolveOption resolveOption, IEqualityComparer<TKey> comparer = null)
         {
             return target.ToNonCollidingDictionary(
                 keySelector,
@@ -68,15 +68,16 @@ namespace Necessity
                 (e, n) =>
                     resolveOption == ResolveOption.Keep
                         ? e
-                        : n);
+                        : n, 
+                comparer);
         }
 
-        public static Dictionary<TKey, TVal> Merge<TKey, TVal>(this IDictionary<TKey, TVal> left, IDictionary<TKey, TVal> right, ResolveOption resolveOption = ResolveOption.Replace)
+        public static Dictionary<TKey, TVal> Merge<TKey, TVal>(this IDictionary<TKey, TVal> left, IDictionary<TKey, TVal> right, ResolveOption resolveOption = ResolveOption.Replace, IEqualityComparer<TKey> comparer = null)
         {
             return left
                 .ToList()
                 .Concat(right.ToList())
-                .ToNonCollidingDictionary(i => i.Key, i => i.Value, resolveOption);
+                .ToNonCollidingDictionary(i => i.Key, i => i.Value, resolveOption, comparer);
         }
     }
 
